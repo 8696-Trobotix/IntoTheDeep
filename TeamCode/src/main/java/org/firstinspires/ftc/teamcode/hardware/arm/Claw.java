103d96cf246bcc09de36408f622a3725c57633b0
ハイドラント <thecomputer314@gmail.com>
2024-10-22 22:33:14 -0500

    Make claw work?
    Currently doesn't move
    
    - Tada

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/arm/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/arm/Claw.java
@@ -3,26 +3,23 @@
 
 package org.firstinspires.ftc.teamcode.hardware.arm;
 
-import static org.firstinspires.ftc.lib.wpilib.commands.Commands.none;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.lib.wpilib.commands.Command;
 import org.firstinspires.ftc.lib.wpilib.commands.SubsystemBase;
 
 public class Claw extends SubsystemBase {
-  //  private final Servo servo;
+  private final Servo servo;
 
   public Claw(OpMode opMode) {
-    //    servo = opMode.hardwareMap.servo.get("servo1");
+    servo = opMode.hardwareMap.servo.get("armServo");
   }
 
   public Command open() {
-    //    return run(() -> servo.setPosition(1));
-    return none();
+    return run(() -> servo.setPosition(1));
   }
 
   public Command close() {
-    //    return run(() -> servo.setPosition(0));
-    return none();
+    return run(() -> servo.setPosition(0));
   }
 }
