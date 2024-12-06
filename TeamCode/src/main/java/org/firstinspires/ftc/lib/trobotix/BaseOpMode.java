9d6780ba891e8ea15ffe6d2905e1e478a532eab1
ハイドラント <thecomputer314@gmail.com>
2024-12-06 16:00:33 -0600

    Document BaseOpMode
    - Tada

--- TeamCode/src/main/java/org/firstinspires/ftc/lib/trobotix/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/lib/trobotix/BaseOpMode.java
@@ -5,7 +5,9 @@ package org.firstinspires.ftc.lib.trobotix;
 
 import com.outoftheboxrobotics.photoncore.Photon;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.lib.wpilib.commands.Command;
 import org.firstinspires.ftc.lib.wpilib.commands.CommandScheduler;
+import org.firstinspires.ftc.lib.wpilib.commands.Subsystem;
 import org.firstinspires.ftc.lib.wpilib.commands.button.Trigger;
 
 /**
@@ -32,8 +34,22 @@ public abstract class BaseOpMode extends LinearOpMode {
     EndableThread.endThreads();
   }
 
+  /**
+   * All startup code is to be implemented in this method.
+   *
+   * <p>This method is for defining {@link Command}s and {@link Subsystem}s to be used by the {@link
+   * CommandScheduler}.
+   *
+   * <p>Any code that needs to run periodically either should be implemented in a {@link Command} or
+   * in {@link Subsystem#periodic()}
+   */
   protected abstract void startup();
 
+  /**
+   * A {@link Trigger} that flips true when "Start" is pressed on the driver station.
+   *
+   * <p>This is recommended for starting autos, with {@link Trigger#onTrue(Command)}
+   */
   protected final Trigger enableTrigger() {
     return enableTrigger;
   }
@@ -41,10 +57,20 @@ public abstract class BaseOpMode extends LinearOpMode {
   private final CommandXboxController primaryController = new CommandXboxController(this, true);
   private final CommandXboxController secondaryController = new CommandXboxController(this, false);
 
+  /**
+   * A {@link CommandXboxController} that wraps gamepad1 to add extra functionality.
+   *
+   * <p>USERS SHOULD NOT USE GAMEPAD1 DIRECTLY, IT WILL NOT UPDATE. USE THIS INSTEAD.
+   */
   protected final CommandXboxController primaryController() {
     return primaryController;
   }
 
+  /**
+   * A {@link CommandXboxController} that wraps gamepad2 to add extra functionality.
+   *
+   * <p>USERS SHOULD NOT USE GAMEPAD2 DIRECTLY, IT WILL NOT UPDATE. USE THIS INSTEAD.
+   */
   protected final CommandXboxController secondaryController() {
     return secondaryController;
   }
